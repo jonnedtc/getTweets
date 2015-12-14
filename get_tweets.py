22a68830197534dc453e28eb5a222ad67fb46e1b
jonnedtc <jonneengelberts@gmail.com>
2015-12-14 19:38:18 +0100

    Print to console instead of a log file

--- get_tweets.py
+++ get_tweets.py
@@ -15,9 +15,6 @@ api = tweepy.API(auth,wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 skipped = 0
 success = 0
 
-# Create log_file file
-log_file = open('log_file.txt', 'w')
-
 # Create empty utf8 file
 result_file = codecs.open("tweets_"+FILENAME+".txt", "w", "utf8")
 
@@ -32,18 +29,17 @@ for line in lines[1:]:
 		status = api.get_status(id)
 	except Exception, e:
 		skipped = skipped + 1
-		log_file.write(str(success+skipped+1)+" ")
-		log_file.write(str(e)+"\n")
+		print (str(success+skipped+1)+" ")
+		print (str(e)+"\n")
 		continue
 	result_file.write(str(status.created_at)+'\t'+str(line))
 	result_file.write(status.text.replace('\n', ' ')+'\n')
 	success = success + 1
 		
-log_file.write("Finished!")
-log_file.write("Succesful: "+str(success))
-log_file.write("Succesful: "+str(skipped))
+print ("Finished!")
+print ("Succesful: "+str(success))
+print ("Succesful: "+str(skipped))
 
 # Close files
 in_file.close()
 result_file.close()
-log_file.close()
